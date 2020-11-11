5dbfdbb06ef6c554ccc01ecbe4248cd34e8b5807
MohamedSalah9972 <mohamed.salah9972@gmail.com>
2020-11-11 13:23:11 +0200

    Update Terminal.java
    Add function of cp command

--- Terminal.java
+++ Terminal.java
@@ -1,5 +1,9 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,5 +17,26 @@ public class Terminal {
     public void rm(Parser p) throws IOException {
         File f = new File(p.getArguments().get(0));
         f.delete();
+    }
+    public void cp(String sourcePath, String destinationPath) {
+        File sourceFile = null;
+        File destinationFile = null;
+        InputStream input = null;
+        OutputStream output = null;
+        try {
+            sourceFile = new File(sourcePath);
+            destinationFile = new File(destinationPath);
+            input = new FileInputStream(sourceFile);
+            output = new FileOutputStream(destinationFile);
+            byte[] buffer = new byte[1024];
+            int numberOfBytes;
+            while((numberOfBytes=input.read(buffer)) > 0){
+                output.write(buffer, 0, numberOfBytes);
+            }
+            input.close();
+            output.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
