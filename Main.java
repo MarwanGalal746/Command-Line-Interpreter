4ba8c322bfa4b1dca736e1f38e05df6fca5a94ad
Marwan Galal <marawangalal84@gmail.com>
2020-11-11 09:06:52 +0200

    rm function

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Parser p = new Parser();
-        p.parse("cd ./\\ \\ asdasda/\\ allo");
-        System.out.println(p.getCmd());
+        p.parse("rm /home/marwan/ggg");
+        Terminal t = new Terminal();
+        t.rm(p);
     }
 }
--- Terminal.java
+++ Terminal.java
@@ -1,6 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Terminal {
 
     public void pwd(Parser p) {
         System.out.println(System.getProperty("user.dir"));
+    }
+
+    public void rm(Parser p) throws IOException {
+        File f = new File(p.getArguments().get(0));
+        f.delete();
     }
 }
